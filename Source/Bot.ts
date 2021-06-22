56011197b3da5e0bcb6d1bd0aec3f045d50c4dc6
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-22 07:03:55 +0530

    Weary of fixing bugs, I'm gonna restart

--- Source/Bot.ts
+++ Source/Bot.ts
@@ -1,13 +1,2 @@
 import { config } from "dotenv";
-import { join } from "path";
 config();
-import RedditMasterClient from "./Base/Client";
-
-new RedditMasterClient()
-	.start({
-		prefix: [
-			"`",
-		],
-		commandDir: join(__dirname, "Commands"),
-		eventDir: join(__dirname, "Events"),
-	});
