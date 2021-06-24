fff429d547c8b33f1cc2ee7440a5d729417df991
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-24 16:36:21 +0530

    Made changes to ping command and util architecture

--- Source/Commands/Client/PingCommand.ts
+++ Source/Commands/Client/PingCommand.ts
@@ -1,5 +1,5 @@
 import { stripIndents } from "common-tags";
-import { Message, MessageEmbed } from "discord.js";
+import { Message } from "discord.js";
 import BaseCommand from "../../Base/BaseCommand";
 import RedditMasterClient from "../../Base/Client";
 
@@ -14,13 +14,13 @@ export default class PingCommand extends BaseCommand {
 	async run(message: Message) {
 		const msg = await message.channel.send("Pinging...");
 
-		const PingEmbed = new MessageEmbed()
+		const PingEmbed = this.client.util.defaultEmbed(message.author)
 			.setTitle("API Latency")
-			.setColor("GREEN")
 			.setDescription(stripIndents`
-				Websocket Ping: ${this.client.ws.ping} ms
-				Discord API Latency: ${(msg.createdTimestamp - message.createdTimestamp)} ms
-			`);
+				🏓 Websocket Ping : ${this.client.ws.ping} ms
+				💞 Discord API Latency: ${(msg.createdTimestamp - message.createdTimestamp)} ms
+			`)
+			.setThumbnail("https://images-ext-2.discordapp.net/external/m5vn5WxGhkXL2o14JXz1jRkp0a15FrmkJkllpZ9f0fI/https/i.giphy.com/media/fvA1ieS8rEV8Y/200.gif?width=240&height=180");
 
 		return message.channel.send(this.client.util.embedFormat(PingEmbed))
 			.then(() => msg.delete());
--- Source/Helpers/Util.ts
+++ Source/Helpers/Util.ts
@@ -1,9 +1,16 @@
-import { MessageEmbed } from "discord.js";
+import { ColorResolvable, MessageEmbed, User } from "discord.js";
 
 export default class Util {
 	public embedFormat(embeds: MessageEmbed | MessageEmbed[]) {
 		if(!Array.isArray(embeds)) embeds = [embeds];
 
 		return { embeds };
+	}
+
+	public defaultEmbed(user: User, colour: ColorResolvable = "GREEN") {
+		return new MessageEmbed()
+			.setTimestamp()
+			.setAuthor(user.tag, user.displayAvatarURL({ dynamic: true }) || user.defaultAvatarURL)
+			.setColor(colour);
 	}
 };
